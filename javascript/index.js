9925a20d0f89ab6f9bf6977901b99e123c4a7d95
Hava Israilova <87033045+HavaIsrailova@users.noreply.github.com>
2022-02-11 20:20:22 +0300

    исправлено

--- javascript/index.js
+++ javascript/index.js
@@ -1,36 +1,29 @@
-
-const popup = document.querySelector('.popup');
-const profileEdit = document.querySelector('.profile__edit');
-const closePopup = document.querySelector('.popup__close-popup');
-
+const popup = document.querySelector(".popup");
+const profileEdit = document.querySelector(".profile__edit");
+const popupClose = document.querySelector(".popup__close-popup");
 const inputUserProfile = document.querySelector('[name="user"]');
 const inputAboutProfile = document.querySelector('[name="about"]');
-
-const profileName = document.querySelector('.profile__name');
-const profileDescription = document.querySelector('.profile__description');
-
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
 const formEdit = document.querySelector('[name="edit-form"]');
-
-// функция открыть 
-function popupOpen(e) {
-	popup.classList.add('popup_opened');
-	inputUserProfile.value = profileName.textContent;
-	inputAboutProfile.value = profileDescription.textContent;
-} 
-// функция закрыть 
-function popupCLose(e) {
-	popup.classList.remove('popup_opened');
-} 
-
+// функция открыть
+function openPopup(e) {
+    popup.classList.add("popup_opened");
+    inputUserProfile.value = profileName.textContent;
+    inputAboutProfile.value = profileDescription.textContent;
+}
+// функция закрыть
+function closePopup(e) {
+    popup.classList.remove("popup_opened");
+}
 // функция сохранения изменений в имени профиля
-function formSubmitHandler(e) {
-	e.preventDefault();
-	profileName.textContent = inputUserProfile.value;
-	profileDescription.textContent = inputAboutProfile.value;
-	popupCLose();
-} 
-
+function handleProfileFormSubmit(e) {
+    e.preventDefault();
+    profileName.textContent = inputUserProfile.value;
+    profileDescription.textContent = inputAboutProfile.value;
+    closePopup();
+}
 // работа функции
-profileEdit.addEventListener('click', popupOpen); // нажатие открытие попапа
-formEdit.addEventListener('submit', formSubmitHandler); // редактирование профиля
-closePopup.addEventListener('click', popupCLose); // закрытие попапа
+profileEdit.addEventListener("click", openPopup); // нажатие открытие попапа
+formEdit.addEventListener("submit", handleProfileFormSubmit); // редактирование профиля
+popupClose.addEventListener("click", closePopup); // закрытие попапа
